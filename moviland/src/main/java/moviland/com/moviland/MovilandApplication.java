588ad6746253d3a00d489cbf71ce79c012647212
Jera <jerameelperezm@gmail.com>
2023-10-18 00:41:58 -0500

    ds

---
+++ moviland/src/main/java/moviland/com/moviland/MovilandApplication.java
@@ -0,0 +1,13 @@
+package moviland.com.moviland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovilandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovilandApplication.class, args);
+	}
+
+}
---
+++ moviland/src/test/java/moviland/com/moviland/MovilandApplicationTests.java
@@ -0,0 +1,13 @@
+package moviland.com.moviland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovilandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
